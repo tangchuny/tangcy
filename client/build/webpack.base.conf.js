0afe802f68e9c90842a1e13afb7ba881cc0b275f
tangchuny <442593738@qq.com>
2018-09-04 12:05:57 +0800

    scss
    scss

--- client/build/webpack.base.conf.js
+++ client/build/webpack.base.conf.js
@@ -76,7 +76,7 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
     ]
   },
   node: {
--- client/src/pages/index/index.js
+++ client/src/pages/index/index.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/index'
 import methods from '../../common/methods.js'
+import './style/index.scss'
+
 Vue.config.productionTip = false
 
 window.Vue = Vue
